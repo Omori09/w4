6f52fceaf96d58da5be21e529325c3f2a1844e18
Omori09 <131243816+Omori09@users.noreply.github.com>
2023-10-12 09:42:50 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -30,6 +30,8 @@ function result() {
   if (customName.value !== '') {
     const name = customName.value;
     newStory = newStory.replace('Bob', name);
+    newStory = newStory.replace('Bob', name);
+    newStory = newStory.replace('Bob', name);
   }
 
   // if (document.getElementById("uk").checked) {
